96b37c37588f94a4bf3ddfb0a48e771359075fbb
seantsao00 <seantsao00@gmail.com>
2024-01-24 22:11:17 +0800

    Add ESlint to back/

--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,43 @@
 import express from 'express'
 import cors from 'cors'
-import { getAuth, DecodedIdToken } from 'firebase-admin/auth'
+import { getAuth, type DecodedIdToken } from 'firebase-admin/auth'
 import { initializeApp } from 'firebase-admin/app'
 import admin from 'firebase-admin'
 
-declare global {
-    namespace Express {
-        interface Request {
-            user?: DecodedIdToken | null;
-        }
-    }
+declare module 'express-serve-static-core' {
+  interface Request {
+    user?: DecodedIdToken
+  }
 }
 
-const auth = getAuth(initializeApp({ credential: admin.credential.cert('./service-account-file.json') }));
-// The file is generated from 
+const firebaseApp = initializeApp({ credential: admin.credential.cert('./service-account-file.json') })
+const auth = getAuth(firebaseApp)
+// The file is generated from
 // https://console.firebase.google.com/project/_/settings/serviceaccounts/adminsdk?hl=zh-tw&_gl=1*z361db*_ga*MTUwNDEzMzY1Ni4xNzA2MDQ1MzY0*_ga_CW55HF8NVT*MTcwNjA5NTQ1My40LjEuMTcwNjA5OTM4MS4yMC4wLjA.
 // Please refer to
 // https://firebase.google.com/docs/admin/setup?hl=zh-tw#initialize_the_sdk_in_non-google_environments
 
-const app = express()
+const expressApp = express()
 const port = 3000
 
-app.use(cors())
+expressApp.use(cors())
 
-app.use(async (req, res, next) => {
-    let token = req.headers.authorization;
-    if (!token) next();
-    // 驗證 token
-    else {
-        try {
-            token = token.split(' ')[1];
-            const decodedToken = await auth.verifyIdToken(token);
-            req.user = decodedToken;
-            console.log(req.user);
-            next();
-        } catch (error) {
-            console.log(error);
-            next();
-        }
-    }
+expressApp.use((req, res, next) => {
+  let token = req.headers.authorization
+  if (token === undefined) next()
+  // 驗證 token
+  else {
+    (async () => {
+      token = token.split(' ')[1]
+      const decodedToken = await auth.verifyIdToken(token)
+      req.user = decodedToken
+      console.log(req.user)
+      next()
+    })().catch((err) => {
+      console.log(err)
+    })
+  }
 })
 
-app.listen(port)
+expressApp.listen(port)
 console.log('Start listening')
