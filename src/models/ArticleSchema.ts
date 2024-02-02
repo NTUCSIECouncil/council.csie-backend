f78597ee4d6142e54605ce8a91485d1ee69508f1
trs4630 <tuanrongshan@gmail.com>
2024-02-02 14:29:00 +0800

    back: slightly modify article schema

--- src/models/ArticleSchema.ts
+++ src/models/ArticleSchema.ts
@@ -2,12 +2,16 @@ import { Schema } from 'mongoose';
 
 interface Article {
   title: string;
+  lecturer: string;
   tag?: string[];
+  content?: string;
 }
 
 const articleSchema = new Schema<Article>({
   title: { type: String, required: true },
-  tag: { type: String, required: false }
+  lecturer: {type: String, required: true},
+  tag: { type: [ { type: String, required: false } ], required: false },
+  content: { type: String, required: false }
 });
 
 export { type Article, articleSchema };
