e8b13aad999ac783d22779bad4297a9f1094743d
trs4630 <tuanrongshan@gmail.com>
2024-02-02 18:55:32 +0800

    feat(back): add router component to ArticleController

--- src/routers/ArticleController.ts
+++ src/routers/ArticleController.ts
@@ -15,4 +15,23 @@ router.get('/', (req, res) => {
   });
 });
 
+// TODO: search articles by keyword
+//       (unchecked)
+router.get('/:keyword', (req, res) => {
+  (async () => {
+    const articles: Article[] = await models.Article.find({
+      $or: [
+        { title: { $regex: req.params.keyword, $options: 'i' } },
+        { lecturer: { $regex: req.params.keyword, $options: 'i' } },
+        { tag: { $regex: req.params.keyword, $options: 'i' } },
+        { content: { $regex: req.params.keyword, $options: 'i' } }
+      ]
+    }).exec();
+    res.json({ result: articles });
+  })().catch((err) => {
+    console.log(err);
+    res.sendStatus(500);
+  });
+});
+
 export default router;
