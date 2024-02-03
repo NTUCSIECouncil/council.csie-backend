d99110e5216bc53ee078258e4374613eba6fff45
seantsao00 <seantsao00@gmail.com>
2024-02-04 02:51:56 +0800

    feat(back): change api of articles search
    Slightly change the form of api to using query params instead of routes params. Also, this change /api/articles back to previous behavior, getting all articles existing.

--- src/routers/ArticleController.ts
+++ src/routers/ArticleController.ts
@@ -1,13 +1,13 @@
-import { Router } from 'express';
+import { Router, type Request } from 'express';
 import { type Article } from '@models/ArticleSchema';
 import { models } from '@models/index';
 
 const router = Router();
 
-// TODO: use such form for articles search?
+// get all articles
 router.get('/', (req, res) => {
   (async () => {
-    const articles = await models.Article.find(req.params).exec();
+    const articles = await models.Article.find().exec();
     res.json({ result: articles });
   })().catch((err) => {
     console.log(err);
@@ -15,19 +15,33 @@ router.get('/', (req, res) => {
   });
 });
 
+// Some interfaces for articles searching
+// TODO: maybe should be moved to a separated file
+
+interface ArticleSearchParams {
+  keyword: string;
+}
+
+interface ArticleSearchResult {
+  results: Article[];
+}
+
+interface ArticleSearchRequest extends Request<null, ArticleSearchResult, null, ArticleSearchParams> {};
+
 // TODO: search articles by keyword
 //       (unchecked)
-router.get('/:keyword', (req, res) => {
+router.post('/search', (req: ArticleSearchRequest, res) => {
   (async () => {
+    const queryParams = req.query;
     const articles: Article[] = await models.Article.find({
       $or: [
-        { title: { $regex: req.params.keyword, $options: 'i' } },
-        { lecturer: { $regex: req.params.keyword, $options: 'i' } },
-        { tag: { $regex: req.params.keyword, $options: 'i' } },
-        { content: { $regex: req.params.keyword, $options: 'i' } }
+        { title: { $regex: queryParams.keyword, $options: 'i' } },
+        { lecturer: { $regex: queryParams.keyword, $options: 'i' } },
+        { tag: { $regex: queryParams.keyword, $options: 'i' } },
+        { content: { $regex: queryParams.keyword, $options: 'i' } }
       ]
     }).exec();
-    res.json({ result: articles });
+    res.json({ results: articles });
   })().catch((err) => {
     console.log(err);
     res.sendStatus(500);
