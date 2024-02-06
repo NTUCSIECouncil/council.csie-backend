79203eb227ad6d3712bc231256b153d38c2f3c13
seantsao00 <seantsao00@gmail.com>
2024-02-07 04:39:43 +0800

    refactor(API): reorganize API of user account
    GET /api/users is removed. To add new user to DB, use PUT /api/users/{uid} instead. Moreover, POST /api/users/{uid} is removed also, because its original role is replaced by PUT /api/users/{uid}.

--- src/routers/UserInfoController.ts
+++ src/routers/UserInfoController.ts
@@ -6,26 +6,67 @@ const router = Router();
 
 const UserModel = models.User;
 
-router.get('/:uid?', (req, res) => {
+router.get('/:uid', (req, res) => {
   (async () => {
-    if (req.params.uid === undefined || req.guser?.uid === req.params.uid) {
-      // access itself
-      if (req.guser?.uid === undefined) {
-        res.sendStatus(403);
+    if (req.guser?.uid === undefined || req.guser?.uid !== req.params.uid) {
+      res.sendStatus(403);
+    } else {
+      const targetUser = await UserModel.findOne({ uid: req.guser.uid }).exec();
+      if (targetUser === null) {
+        // If not found, return status 404
+        // In this case, expect recourse be created by PUT soon after
+        res.sendStatus(404);
       } else {
-        let targetUser = await UserModel.findOne({ uid: req.guser.uid });
-        if (targetUser == null) {
-          targetUser = new UserModel({
-            uid: req.guser.uid,
-            name: req.guser.name,
-            email: req.guser.email
-          });
-          await targetUser.save();
-        }
         res.json(targetUser.toJSON());
       }
-    } else {
+    }
+    // if (req.params.uid === undefined || req.guser?.uid === req.params.uid) {
+    //   // access itself
+    //   if (req.guser?.uid === undefined) {
+    //     res.sendStatus(403);
+    //   } else {
+    //     let targetUser = await UserModel.findOne({ uid: req.guser.uid }).exec();
+    //     if (targetUser == null) {
+    //       targetUser = new UserModel({
+    //         uid: req.guser.uid,
+    //         name: req.guser.name,
+    //         email: req.guser.email
+    //       });
+    //       await targetUser.save();
+    //     }
+    //     res.json(targetUser.toJSON());
+    //   }
+    // } else {
+    //   res.sendStatus(403);
+    // }
+  })().catch((err) => {
+    console.log(err);
+    res.sendStatus(500);
+  });
+});
+
+router.put('/:uid', (req, res) => {
+  (async () => {
+    if (req.guser?.uid === undefined || req.guser?.uid !== req.params.uid) {
       res.sendStatus(403);
+    } else {
+      const newInfo: Partial<User> = req.body;
+      let targetUser = await UserModel.findOne({ uid: req.guser.uid }).exec();
+      if (targetUser !== null) {
+        targetUser.set(newInfo); // properties not in User will not be store into document
+        await targetUser.save();
+        res.sendStatus(204);
+      } else {
+        // If the target user does currently not exist, create it
+        targetUser = new UserModel({
+          uid: req.guser.uid,
+          name: req.guser.name,
+          email: req.guser.email
+        });
+        targetUser.set(newInfo);
+        await targetUser.save();
+        res.sendStatus(201);
+      }
     }
   })().catch((err) => {
     console.log(err);
@@ -33,21 +74,16 @@ router.get('/:uid?', (req, res) => {
   });
 });
 
-// TODO: did not check if req.body is an implementation of IUser
-// router.put('/:uid', (req, res) => {
+// TODO: did not check if user already exist, also did not check req.body
+//       也許應該用監聽 firebase 之類的方法創建帳號
+// Update: 使用 PUT 建立帳號，應該不需要了
+// router.post('/:uid', (req, res) => {
 //   (async () => {
 //     if (req.guser?.uid !== undefined && req.guser?.uid === req.params.uid) {
-//       const targetUser = await UserModel.findOne({ uid: req.guser?.uid }).exec();
-//       if (targetUser !== null) {
-//         const newInfo: IUser = req.body;
-//         let prop: keyof IUser;
-//         for (prop in newInfo) {
-//           targetUser[prop] = newInfo[prop];
-//         }
-//         res.sendStatus(204);
-//       } else {
-//         res.sendStatus(400);
-//       }
+//       const info: User = req.body;
+//       const newUser = new UserModel(info);
+//       await newUser.save();
+//       res.sendStatus(204);
 //     } else {
 //       res.sendStatus(403);
 //     }
@@ -56,23 +92,5 @@ router.get('/:uid?', (req, res) => {
 //     res.sendStatus(500);
 //   });
 // });
-
-// TODO: did not check if user already exist, also did not check req.body
-//       也許應該用監聽 firebase 之類的方法創建帳號
-router.post('/:uid', (req, res) => {
-  (async () => {
-    if (req.guser?.uid !== undefined && req.guser?.uid === req.params.uid) {
-      const info: User = req.body;
-      const newUser = new UserModel(info);
-      await newUser.save();
-      res.sendStatus(204);
-    } else {
-      res.sendStatus(403);
-    }
-  })().catch((err) => {
-    console.log(err);
-    res.sendStatus(500);
-  });
-});
 
 export default router;
