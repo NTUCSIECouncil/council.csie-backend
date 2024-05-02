dc721d8270e9301a9a73c1dbc6c656ea8981a1cc
Lily <yenshanchen.tw@gmail.com>
2024-05-02 22:24:23 +0800

    fix(front): fix article attributes and search bar linter

--- src/models/ArticleSchema.ts
+++ src/models/ArticleSchema.ts
@@ -2,25 +2,27 @@ import { Schema, SchemaTypes } from 'mongoose';
 import { type User } from '@models/UserSchema';
 
 interface Article {
+  id: number;
   title: string;
   lecturer: string;
   tag?: string[];
   grade?: number;
   categories?: string[];
   content?: string;
-  creator: User;
+  creator?: User;
   createdAt?: Date;
   updatedAt?: Date;
 }
 
 const articleSchema = new Schema<Article>({
+  id: { type: Number, required: true },
   title: { type: String, required: true },
   lecturer: { type: String, required: true },
   tag: { type: [{ type: String, required: false }], required: false },
   grade: { type: Number, required: false },
   categories: { type: [{ type: String, required: false }], required: false },
   content: { type: String, required: false },
-  creator: { type: SchemaTypes.ObjectId, ref: 'User', required: true },
+  creator: { type: SchemaTypes.ObjectId, ref: 'User', required: false },
   createdAt: { type: Date, required: false, immutable: true, default: () => Date.now() },
   updatedAt: { type: Date, required: false, default: () => Date.now() }
 });
