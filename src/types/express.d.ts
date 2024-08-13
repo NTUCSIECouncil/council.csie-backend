53bea9bf0fc9748ef970ea3efcdfeb7413cc2569
seantsao00 <seantsao00@gmail.com>
2024-08-14 01:48:46 +0800

    refactor(back/type): use a more reasonable way extend express.js Request interface

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,12 +1,11 @@
 import { type DecodedIdToken } from 'firebase-admin/auth';
 
-declare module 'express-serve-static-core' {
-  interface Request {
-    guser?: DecodedIdToken;
-    portionSize?: number;
-    portionNum?: number;
-  }
-  interface Response {
-    createTime?: string;
+declare global {
+  declare namespace Express {
+    export interface Request {
+      guser?: DecodedIdToken;
+      portionSize?: number;
+      portionNum?: number;
+    }
   }
 }
