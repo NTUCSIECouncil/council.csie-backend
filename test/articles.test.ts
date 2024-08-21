7cce2abdc7155d7c833e839812ad6b6f0cb79bea
seantsao00 <seantsao00@gmail.com>
2024-08-22 02:24:53 +0800

    test(articles test): add more test and rewrite test messages

--- test/articles.test.ts
+++ test/articles.test.ts
@@ -2,77 +2,141 @@ import mongoose from 'mongoose';
 import { describe, beforeEach, afterEach, expect, it } from '@jest/globals';
 import qs from 'qs';
 import request from 'supertest';
+import { ArticleSearchQueryParam, PaginationQueryParam } from '@models/util-schema.ts';
 import app from './app.ts';
 import { insertFromFile } from './utils.ts';
 
-describe('Article', function () {
-  beforeEach(async () => {
-    await insertFromFile('Article');
-    await insertFromFile('User');
+beforeEach(async () => {
+  await insertFromFile('Article');
+  await insertFromFile('User');
+});
+
+afterEach(async () => {
+  await mongoose.connection.db?.dropDatabase();
+});
+
+describe('GET /api/articles', () => {
+  it('should return the first page', async () => {
+    const res = await request(app)
+      .get('/api/articles/')
+      .expect(200);
+    expect(res.body.data).toHaveLength(10);
   });
 
-  afterEach(async () => {
-    await mongoose.connection.db?.dropDatabase();
+  it('should support controlling the offset of page', async () => {
+    let res = await request(app)
+      .get('/api/articles?' + qs.stringify({ offset: 1 }))
+      .expect(200);
+    expect(res.body.data).toHaveLength(10);
+
+    res = await request(app)
+      .get('/api/articles?' + qs.stringify({ offset: 20 }))
+      .expect(200);
+    expect(res.body.data).toHaveLength(1);
+
+    res = await request(app)
+      .get('/api/articles?' + qs.stringify({ offset: 21 }))
+      .expect(400);
   });
 
-  describe('GET requests', () => {
-    it('/api/articles', async () => {
-      const res = await request(app)
-        .get('/api/articles/')
-        .expect(200);
-      expect(res.body.data).toHaveLength(10);
-    });
+  it('should support controlling the limit size of page', async () => {
+    let res = await request(app)
+      .get('/api/articles?' + qs.stringify({ limit: 1 }))
+      .expect(200);
+    expect(res.body.data).toHaveLength(1);
 
-    it('/api/articles - portionNum', async () => {
-      let res = await request(app)
-        .get('/api/articles?' + qs.stringify({ portionNum: 1 }))
-        .expect(200);
-      expect(res.body.data).toHaveLength(10);
-      res = await request(app)
-        .get('/api/articles?' + qs.stringify({ portionNum: 2 }))
-        .expect(200);
-      expect(res.body.data).toHaveLength(1);
-      res = await request(app)
-        .get('/api/articles?' + qs.stringify({ portionNum: 3 }))
-        .expect(400);
-    });
+    res = await request(app)
+      .get('/api/articles?' + qs.stringify({ limit: 5 }))
+      .expect(200);
+    expect(res.body.data).toHaveLength(5);
 
-    it('/api/articles - portionSize', async () => {
-      let res = await request(app)
-        .get('/api/articles?' + qs.stringify({ portionSize: 20 }))
-        .expect(200);
-      expect(res.body.data).toHaveLength(20);
-      res = await request(app)
-        .get('/api/articles?' + qs.stringify({ portionSize: 50 }))
-        .expect(200);
-      expect(res.body.data).toHaveLength(21);
-      res = await request(app)
-        .get('/api/articles?' + qs.stringify({ portionSize: 100 }))
-        .expect(200);
-      expect(res.body.data).toHaveLength(21);
-      res = await request(app)
-        .get('/api/articles?' + qs.stringify({ portionSize: 21 }))
-        .expect(400);
-    });
+    res = await request(app)
+      .get('/api/articles?' + qs.stringify({ limit: 20 }))
+      .expect(200);
+    expect(res.body.data).toHaveLength(20);
 
-    it('/api/articles/search', async () => {
-      const queryOne = {
-        tag: ['德邦讚'],
-      };
-      const resOne = await request(app)
-        .get('/api/articles/search?' + qs.stringify(queryOne))
-        .expect(200);
+    res = await request(app)
+      .get('/api/articles?' + qs.stringify({ limit: 21 }))
+      .expect(200);
+    expect(res.body.data).toHaveLength(21);
 
-      expect(resOne.body.data).toHaveLength(10);
-      const queryTwo = {
-        tag: ['德邦讚'],
-        portionNum: 1,
-      };
-      const resTwo = await request(app)
-        .get('/api/articles/search?' + qs.stringify(queryTwo))
-        .expect(200);
+    res = await request(app)
+      .get('/api/articles?' + qs.stringify({ limit: 50 }))
+      .expect(200);
+    expect(res.body.data).toHaveLength(21);
+  });
 
-      expect(resTwo.body.data).toHaveLength(4);
-    });
+  it('should support controlling both the offset and the limit size of page', async () => {
+    let res = await request(app)
+      .get('/api/articles?' + qs.stringify({ limit: 1, offset: 1 }))
+      .expect(200);
+    expect(res.body.data).toHaveLength(1);
+
+    res = await request(app)
+      .get('/api/articles?' + qs.stringify({ limit: 1, offset: 20 }))
+      .expect(200);
+    expect(res.body.data).toHaveLength(1);
+
+    res = await request(app)
+      .get('/api/articles?' + qs.stringify({ limit: 5, offset: 1 }))
+      .expect(200);
+    expect(res.body.data).toHaveLength(5);
+
+    res = await request(app)
+      .get('/api/articles?' + qs.stringify({ limit: 5, offset: 19 }))
+      .expect(200);
+    expect(res.body.data).toHaveLength(2);
+  });
+});
+
+describe('GET /api/articles/search', () => {
+  it('should return the search result', async () => {
+    const query: ArticleSearchQueryParam | PaginationQueryParam = {
+      tag: ['德邦讚'],
+    };
+    const res = await request(app)
+      .get('/api/articles/search?' + qs.stringify(query))
+      .expect(200);
+
+    expect(res.body.data).toHaveLength(10);
+  });
+
+  it('should support pagination', async () => {
+    let query: ArticleSearchQueryParam | PaginationQueryParam = {
+      tag: ['德邦讚'],
+      offset: 10,
+    };
+    let res = await request(app)
+      .get('/api/articles/search?' + qs.stringify(query))
+      .expect(200);
+    expect(res.body.data).toHaveLength(4);
+
+    query = {
+      tag: ['德邦讚'],
+      limit: 14,
+    };
+    res = await request(app)
+      .get('/api/articles/search?' + qs.stringify(query))
+      .expect(200);
+    expect(res.body.data).toHaveLength(14);
+
+    query = {
+      tag: ['德邦讚'],
+      limit: 15,
+    };
+    res = await request(app)
+      .get('/api/articles/search?' + qs.stringify(query))
+      .expect(200);
+    expect(res.body.data).toHaveLength(14);
+
+    query = {
+      tag: ['德邦讚'],
+      offset: 10,
+      limit: 3,
+    };
+    res = await request(app)
+      .get('/api/articles/search?' + qs.stringify(query))
+      .expect(200);
+    expect(res.body.data).toHaveLength(3);
   });
 });
