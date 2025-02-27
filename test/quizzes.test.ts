8ee614575140341ed07125f622c0d900ed728128
JerryTsai <b11902040@csie.ntu.edu.tw>
2025-02-27 16:47:41 +0800

    feat(test): add quiz file and update tests for file retrieval

--- test/quizzes.test.ts
+++ test/quizzes.test.ts
@@ -6,6 +6,7 @@ import app from './app.ts';
 import { insertFromFile } from './utils.ts';
 
 beforeEach(async () => {
+  process.env.QUIZ_FILE_DIR = 'test/dummy-data/quiz_file_samples';
   await insertFromFile('Course');
   await insertFromFile('Quiz');
 });
@@ -15,47 +16,53 @@ afterEach(async () => {
 });
 
 describe('GET /api/quizzes', () => {
-  it('should response the first page', async () => {
+  it('should response the first page and receive at most 10 quizzes', async () => {
     const res = await request(app)
       .get('/api/quizzes/')
       .expect(200);
-    expect(res.body.items).toHaveLength(5);
+    expect(res.body.items).toHaveLength(10); // default limit is 10
   });
 
   it('should support controlling both the offset and the limit size of page', async () => {
+    // adjust the limit and offset
     let res = await request(app)
       .get('/api/quizzes')
       .query({ limit: 1, offset: 1 })
       .expect(200);
     expect(res.body.items).toHaveLength(1);
 
+    // the offset is out of range (there are only 100 quizzes)
     res = await request(app)
       .get('/api/quizzes')
-      .query({ limit: 1, offset: 20 })
+      .query({ limit: 1, offset: 200 })
       .expect(200);
     expect(res.body.items).toHaveLength(0);
 
+    // the limit is out of range (there are only 100 quizzes)
+    // should retun the remaining quizzes(1)
     res = await request(app)
       .get('/api/quizzes')
-      .query({ limit: 5, offset: 1 })
+      .query({ limit: 10, offset: 99 })
       .expect(200);
-    expect(res.body.items).toHaveLength(4);
+    expect(res.body.items).toHaveLength(1);
 
+    // large limit
     res = await request(app)
       .get('/api/quizzes')
-      .query({ limit: 3, offset: 1 })
+      .query({ limit: 200, offset: 0 })
       .expect(200);
-    expect(res.body.items).toHaveLength(3);
+    expect(res.body.items).toHaveLength(100);
   });
 });
 
 describe('POST /api/quizzes', () => {
   it('should create a quiz', async () => {
+    // create a quiz
     let res = await request(app)
       .post('/api/quizzes')
       .send({
         course: '00000003-0003-0000-0000-000000000000',
-        uploader: '00000002-0000-0000-0000-000000000000',
+        uploader: '00000001-0001-0000-0000-000000000000',
         semester: '112-1',
         session: 'midterm',
       })
@@ -68,7 +75,7 @@ describe('POST /api/quizzes', () => {
       .expect(200);
     expect(res.body.item).toMatchObject({
       course: '00000003-0003-0000-0000-000000000000',
-      uploader: '00000002-0000-0000-0000-000000000000',
+      uploader: '00000001-0001-0000-0000-000000000000',
       semester: '112-1',
       session: 'midterm',
     });
@@ -112,13 +119,13 @@ describe('POST /api/quizzes', () => {
 describe('GET /api/quizzes/:uuid', () => {
   it('should response the quiz with uuid', async () => {
     const res = await request(app)
-      .get('/api/quizzes/00000004-0002-0000-0000-000000000000')
+      .get('/api/quizzes/00000004-1131-0000-0000-000000000000')
       .expect(200);
     expect(res.body.item).toMatchObject({
-      _id: '00000004-0002-0000-0000-000000000000',
-      course: '00000003-0003-0000-0000-000000000000',
-      uploader: '00000002-0000-0000-0000-000000000000',
-      semester: '112-1',
+      _id: '00000004-1131-0000-0000-000000000000',
+      course: '00000003-0000-0000-0000-000000000000',
+      uploader: '00000001-0003-0000-0000-000000000000',
+      semester: '113-1',
       session: 'midterm',
     });
 
@@ -134,15 +141,23 @@ describe('GET /api/quizzes/:uuid', () => {
 
 describe('GET /api/quizzes/:uuid/file', () => {
   it('should response the quiz file', async () => {
+    // the file exists
     const res = await request(app)
-      .get('/api/quizzes/00000004-0001-0000-0000-000000000000/file')
+      .get('/api/quizzes/00000004-1131-0000-0000-000000000000/file')
       .expect(200);
     expect(res.type).toEqual('application/pdf');
 
+    // the uuid does not exist
     await request(app)
       .get('/api/quizzes/00000004-0000-0000-0000-000000000000/file')
       .expect(404);
 
+    // the uuid exist but the file does not
+    await request(app)
+      .get('/api/quizzes/00000004-1131-0001-0000-000000000000/file')
+      .expect(404);
+
+    // invalid uuid (wrong format)
     await request(app)
       .get('/api/quizzes/00000004-0000-0000-0000/file')
       .expect(400);
@@ -151,8 +166,9 @@ describe('GET /api/quizzes/:uuid/file', () => {
 
 describe('GET /api/quizzes/search', () => {
   it('should response the search result', async () => {
+    // search only by course
     let query: QuizSearchParam | PaginationQueryParam = {
-      course: '00000003-0001-0000-0000-000000000000',
+      course: '00000003-0000-0000-0000-000000000000',
     };
     let res = await request(app)
       .get('/api/quizzes/search')
@@ -160,15 +176,26 @@ describe('GET /api/quizzes/search', () => {
       .expect(200);
     expect(res.body);
 
+    // course does not exist
     query = {
       course: '00000003-0001-0000-0000-000000000000',
-      keyword: '111-2',
     };
     res = await request(app)
       .get('/api/quizzes/search')
       .query(query)
       .expect(200);
-    expect(res.body.items).toHaveLength(1);
+    expect(res.body.items).toHaveLength(0);
+
+    // search by course and semester
+    query = {
+      course: '00000003-0001-0000-0000-000000000000',
+      keyword: '113-1',
+    };
+    res = await request(app)
+      .get('/api/quizzes/search')
+      .query(query)
+      .expect(200);
+    expect(res.body.items).toHaveLength(4);
 
     query = {
       limit: 2,
@@ -176,7 +203,8 @@ describe('GET /api/quizzes/search', () => {
     res = await request(app)
       .get('/api/quizzes/search')
       .query(query)
-      .expect(400);
+      .expect(200);
+    expect(res.body.items).toHaveLength(2);
   });
 
   it('should support pagination', async () => {
