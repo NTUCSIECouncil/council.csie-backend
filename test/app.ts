22f5290e600c334334fabce26263f7709745f087
seantsao00 <seantsao00@gmail.com>
2025-03-13 00:23:24 +0800

    feat(app): set query parser to extended in test app also

--- test/app.ts
+++ test/app.ts
@@ -4,6 +4,8 @@ import APIController from '@routers/API-controller.ts';
 
 const expressApp = express();
 
+expressApp.set('query parser', 'extended');
+
 process.env.QUIZ_FILE_DIR = 'uploads/quizzes';
 
 expressApp.use((req, res, next) => {
