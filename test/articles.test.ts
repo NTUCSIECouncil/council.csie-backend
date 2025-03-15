7b4bb2a19f38d89f48f912c239ffee2ce4d7a548
seantsao00 <seantsao00@gmail.com>
2025-03-16 04:19:58 +0800

    test(article): rewrite the E2E test of /api/articles for better coverage and readability

--- test/articles.test.ts
+++ test/articles.test.ts
@@ -1,18 +1,18 @@
+import { type UUID, randomUUID } from 'crypto';
 import { afterEach, beforeEach, describe, expect, it } from '@jest/globals';
 import Fuse from 'fuse.js';
 import mongoose from 'mongoose';
 import qs from 'qs';
 import request from 'supertest';
+import { ZArticleSchema } from '@models/article-schema.ts';
 import { type Course } from '@models/course-schema.ts';
 import { models } from '@models/index.ts';
-import { type ArticleSearchQueryParam, type PaginationQueryParam, ZUuidSchema } from '@models/util-schema.ts';
+import { ZUuidSchema } from '@models/util-schema.ts';
 import app from './app.ts';
 import { insertFromFile } from './utils.ts';
 
 beforeEach(async () => {
   await insertFromFile('Article');
-  await insertFromFile('Course');
-  await insertFromFile('User');
 });
 
 afterEach(async () => {
@@ -20,109 +20,51 @@ afterEach(async () => {
 });
 
 describe('GET /api/articles', () => {
-  it('should response the first page', async () => {
+  it('should response exact schema of Article', async () => {
     const res = await request(app)
-      .get('/api/articles/')
+      .get('/api/articles')
+      .query({ limit: 1 })
       .expect(200);
-    expect(res.body.items).toHaveLength(10);
+    expect(ZArticleSchema.safeParse(res.body.items[0]).success).toBe(true);
   });
 
-  it('should support controlling the offset of page', async () => {
+  it('should support pagination', async () => {
+    for (const offset of [0, 1, 99, 100, 101]) {
+      for (const limit of [1, 5, 10, 20, 100, 105]) {
+        const res = await request(app)
+          .get('/api/articles')
+          .query({ limit, offset })
+          .expect(200);
+        expect(res.body.items).toHaveLength(Math.max(0, Math.min(100 - offset, limit)));
+      }
+    }
+
     let res = await request(app)
       .get('/api/articles')
-      .query({ offset: 1 })
+      .query({ limit: 10, offset: 0 })
       .expect(200);
-    expect(res.body.items).toHaveLength(10);
+    const page = ZArticleSchema.array().parse(res.body.items);
 
     res = await request(app)
       .get('/api/articles')
-      .query({ offset: 99 })
       .expect(200);
-    expect(res.body.items).toHaveLength(1);
-
-    res = await request(app)
-      .get('/api/articles')
-      .query(({ offset: 100 }))
-      .expect(200);
-    expect(res.body.items).toHaveLength(0);
-
-    res = await request(app)
-      .get('/api/articles')
-      .query(({ offset: 101 }))
-      .expect(200);
-    expect(res.body.items).toHaveLength(0);
-  });
-
-  it('should support controlling the limit size of page', async () => {
-    let res = await request(app)
-      .get('/api/articles')
-      .query(({ limit: 1 }))
-      .expect(200);
-    expect(res.body.items).toHaveLength(1);
-
-    res = await request(app)
-      .get('/api/articles')
-      .query(({ limit: 5 }))
-      .expect(200);
-    expect(res.body.items).toHaveLength(5);
-
-    res = await request(app)
-      .get('/api/articles')
-      .query(({ limit: 20 }))
-      .expect(200);
-    expect(res.body.items).toHaveLength(20);
-
-    res = await request(app)
-      .get('/api/articles')
-      .query(({ limit: 100 }))
-      .expect(200);
-    expect(res.body.items).toHaveLength(100);
-
-    res = await request(app)
-      .get('/api/articles')
-      .query(({ limit: 105 }))
-      .expect(200);
-    expect(res.body.items).toHaveLength(100);
-  });
-
-  it('should support controlling both the offset and the limit size of page', async () => {
-    let res = await request(app)
-      .get('/api/articles')
-      .query(({ limit: 1, offset: 1 }))
-      .expect(200);
-    expect(res.body.items).toHaveLength(1);
-
-    res = await request(app)
-      .get('/api/articles')
-      .query(({ limit: 1, offset: 20 }))
-      .expect(200);
-    expect(res.body.items).toHaveLength(1);
-
-    res = await request(app)
-      .get('/api/articles')
-      .query({ limit: 5, offset: 1 })
-      .expect(200);
-    expect(res.body.items).toHaveLength(5);
-
-    res = await request(app)
-      .get('/api/articles')
-      .query(qs.stringify({ limit: 5, offset: 98 }))
-      .expect(200);
-    expect(res.body.items).toHaveLength(2);
+    expect(res.body.items).toStrictEqual(page);
   });
 });
 
 describe('POST /api/articles', () => {
   it('should create an article', async () => {
+    const article = {
+      course: '00000003-0003-0000-0000-000000000000',
+      creator: '00000001-0003-0000-0000-000000000000',
+      semester: '113-2',
+      title: '普通生物學',
+      tags: ['耶'],
+    };
+
     let res = await request(app)
       .post('/api/articles')
-      .send({
-        course: '00000003-0003-0000-0000-000000000000',
-        creator: '00000001-0003-0000-0000-000000000000',
-        semester: '113-2',
-        title: '普通生物學',
-        tags: ['耶'],
-      })
+      .send(article)
       .expect(201);
 
     const uuid = ZUuidSchema.parse(res.body.uuid);
@@ -130,38 +72,37 @@ describe('POST /api/articles', () => {
     res = await request(app)
       .get(`/api/articles/${uuid}`)
       .expect(200);
-    expect(res.body.item).toMatchObject({
-      _id: uuid,
+
+    expect(res.body.item).toStrictEqual({ ...article, _id: uuid });
+  });
+
+  it('should reject invalid article', async () => {
+    const article = {
       course: '00000003-0003-0000-0000-000000000000',
       creator: '00000001-0003-0000-0000-000000000000',
       semester: '113-2',
       title: '普通生物學',
       tags: ['耶'],
-    });
+    };
 
-    res = await request(app)
+    await request(app)
       .post('/api/articles')
-      .send({
-        couse: '00000003-0003-0000-0000-000000000000',
-        creator: '00000001-0003-0000-0000-000000000000',
-        semester: '113-2',
-        title: '普通生物學',
-        tags: ['耶'],
-      })
+      .send({ ...article, course: undefined })
       .expect(400);
   });
 
   it('should ignore provided uuid', async () => {
+    const article = {
+      course: '00000003-0003-0000-0000-000000000000',
+      creator: '00000001-0003-0000-0000-000000000000',
+      semester: '113-2',
+      title: '普通生物學',
+      tags: ['耶'],
+    };
+
     let res = await request(app)
       .post('/api/articles')
-      .send({
-        _id: '00000002-0022-0000-0000-000000000000',
-        course: '00000003-0003-0000-0000-000000000000',
-        creator: '00000001-0003-0000-0000-000000000000',
-        semester: '113-2',
-        title: '普通生物學',
-        tags: ['耶'],
-      })
+      .send({ ...article, _id: '00000002-0022-0000-0000-000000000000' })
       .expect(201);
 
     const uuid = ZUuidSchema.parse(res.body.uuid);
@@ -169,14 +110,7 @@ describe('POST /api/articles', () => {
     res = await request(app)
       .get(`/api/articles/${uuid}`)
       .expect(200);
-    expect(res.body.item).toMatchObject({
-      _id: uuid,
-      course: '00000003-0003-0000-0000-000000000000',
-      creator: '00000001-0003-0000-0000-000000000000',
-      semester: '113-2',
-      title: '普通生物學',
-      tags: ['耶'],
-    });
+    expect(res.body.item).toStrictEqual({ ...article, _id: uuid });
 
     expect(uuid).not.toEqual('00000002-0022-0000-0000-000000000000');
   });
@@ -185,43 +119,26 @@ describe('POST /api/articles', () => {
 describe('GET /api/articles/:uuid', () => {
   it('should response the article with uuid', async () => {
     let res = await request(app)
-      .get('/api/articles/00000002-1131-0000-0000-000000000000')
+      .get('/api/articles')
+      .query({ limit: 1 })
       .expect(200);
-    expect(res.body.item).toMatchObject({
-      _id: '00000002-1131-0000-0000-000000000000',
-      course: '00000003-0000-0000-0000-000000000000',
-      creator: '00000001-0002-0000-0000-000000000000',
-      semester: '113-1',
-      title: '大學國文：文學鑑賞與寫作（一）',
-      tags: [
-        '汪詩珮',
-        '大學國文：文學鑑賞與寫作（一）',
-        'CHIN',
-      ],
-    });
+    const article = ZArticleSchema.parse(res.body.items[0]);
 
     res = await request(app)
-      .get('/api/articles/00000002-1131-0000-0000-000000000098')
+      .get(`/api/articles/${article._id}`)
       .expect(200);
-    expect(res.body.item).toMatchObject({
-      _id: '00000002-1131-0000-0000-000000000098',
-      course: '00000003-0000-0000-0000-000000000098',
-      creator: '00000001-0001-0000-0000-000000000000',
-      semester: '113-1',
-      title: '英文(附一小時英聽)一',
-      tags: [
-        '黃允蔚',
-        '英文(附一小時英聽)一',
-        'FL',
-      ],
-    });
+    expect(res.body.item).toStrictEqual(article);
+  });
 
-    res = await request(app)
+  it('should reject invalid uuid', async () => {
+    await request(app)
       .get('/api/articles/00000002-0003-0000-0000')
       .expect(400);
+  });
 
-    res = await request(app)
-      .get('/api/articles/00000000-0000-0000-0000-000000000000')
+  it('should reject non-exist uuid', async () => {
+    await request(app)
+      .get(`/api/articles/${randomUUID()}`)
       .expect(404);
   });
 });
@@ -229,84 +146,92 @@ describe('GET /api/articles/:uuid', () => {
 describe('PATCH /api/articles/:uuid', () => {
   it('should update the article with uuid', async () => {
     let res = await request(app)
-      .get('/api/articles/00000002-1131-0000-0000-000000000000')
+      .get('/api/articles')
+      .query({ limit: 1 })
       .expect(200);
-    expect(res.body.item).toMatchObject({
-      _id: '00000002-1131-0000-0000-000000000000',
-      course: '00000003-0000-0000-0000-000000000000',
-      creator: '00000001-0002-0000-0000-000000000000',
-      semester: '113-1',
-      title: '大學國文：文學鑑賞與寫作（一）',
-      tags: [
-        '汪詩珮',
-        '大學國文：文學鑑賞與寫作（一）',
-        'CHIN',
-      ],
-    });
+    const article = ZArticleSchema.parse(res.body.items[0]);
 
-    res = await request(app)
-      .patch('/api/articles/00000002-1131-0000-0000-000000000000')
+    await request(app)
+      .patch(`/api/articles/${article._id}`)
       .send({
         title: '不普通物理學',
       })
       .expect(204);
 
     res = await request(app)
-      .patch('/api/articles/00000002-0001-0000-0000')
+      .get(`/api/articles/${article._id}`)
+      .expect(200);
+    expect(res.body.item).toStrictEqual({ ...article, title: '不普通物理學' });
+  });
+
+  it('should reject invalid uuid', async () => {
+    await request(app)
+      .patch('/api/articles/00000002-0003-0000-0000')
       .send({
         title: '不普通物理學',
       })
       .expect(400);
+  });
 
-    res = await request(app)
-      .patch('/api/articles/00000002-1131-0000-0000-000000000000')
+  it('should reject non-exist uuid', async () => {
+    await request(app)
+      .patch(`/api/articles/${randomUUID()}`)
       .send({
         title: '不普通物理學',
       })
-      .expect(204);
+      .expect(400);
   });
 
   it('should reject modification of _id', async () => {
+    const res = await request(app)
+      .get('/api/articles')
+      .query({ limit: 1 })
+      .expect(200);
+    const article = ZArticleSchema.parse(res.body.items[0]);
+
     await request(app)
-      .patch('/api/articles/00000002-0001-0000-0000-000000000000')
+      .patch(`/api/articles/${article._id}`)
       .send({
-        _id: '00000002-0022-0000-0000-000000000000',
+        _id: randomUUID(),
       })
       .expect(400);
   });
 });
 
 describe('GET /api/articles/search', () => {
-  it('should response the search results', async () => {
-    let query: ArticleSearchQueryParam & PaginationQueryParam = {
-      tags: ['CHIN'],
-      limit: 10,
-    };
-    let res = await request(app)
-      .get('/api/articles/search')
-      .query(qs.stringify(query))
-      .expect(200);
-    expect(res.body.items).toHaveLength(10);
+  beforeEach(async () => {
+    await insertFromFile('Course');
+  });
 
-    query = {
-      tags: ['汪詩珮'],
-    };
-    res = await request(app)
-      .get('/api/articles/search')
-      .query(qs.stringify(query))
-      .expect(200);
-    expect(res.body.items).toHaveLength(1);
-    expect(res.body.items[0].tags).toContain('汪詩珮');
+  it('should support search with tag list', async () => {
+    for (const tags of [
+      ['CHIN'], ['汪詩珮'], ['文學'],
+      ['CHIN', '汪詩珮'], ['CHIN', '文學'], ['汪詩珮', '文學'],
+      ['CHIN', '汪詩珮', '文學'],
+    ]) {
+      const res = await request(app)
+        .get('/api/articles/search')
+        .query(qs.stringify({ tags }))
+        .expect(200);
+      for (const article of res.body.items) {
+        expect(article.tags).toEqual(expect.arrayContaining(tags));
+      }
+    }
+  });
 
-    query = {
-      categories: ['General'],
-    };
-    res = await request(app)
-      .get('/api/articles/search')
-      .query(qs.stringify(query))
-      .expect(200);
-    expect(res.body.items).toHaveLength(0);
+  it('should support search with category list', async () => {
+    for (const categories of [['General']]) {
+      const res = await request(app)
+        .get('/api/articles/search')
+        .query(qs.stringify({ categories }))
+        .expect(200);
+      for (const article of res.body.items) {
+        expect(article.course.categories).toEqual(expect.arrayContaining(categories));
+      }
+    }
+  });
 
+  it('should support search in title, course name, and lecturer with keyword', async () => {
     const fuseOptions = {
       keys: [
         'title',
@@ -315,92 +240,68 @@ describe('GET /api/articles/search', () => {
       ],
       threshold: 0.6,
     };
-    const ArticleModel = models.Article;
-    const articles = await ArticleModel.find().populate<{ course: Course }>('course').exec();
-    const fuse = new Fuse(articles, fuseOptions);
+    const populatedArticles = await models.Article.find().populate<{ course: Course }>('course').exec();
+    const fuse = new Fuse(populatedArticles, fuseOptions);
 
-    query = {
-      keyword: '大學國文',
-      limit: 100,
-    };
-    res = await request(app)
-      .get('/api/articles/search')
-      .query(qs.stringify(query))
-      .expect(200);
-    if (query.keyword) {
-      const result = fuse.search(query.keyword);
-      expect(res.body.items).toStrictEqual(result.map(({ item }) => item.depopulate<{ course: string }>().toObject()));
-    }
-
-    query = {
-      keyword: '汪詩珮',
-      limit: 100,
-    };
-    res = await request(app)
-      .get('/api/articles/search')
-      .query(qs.stringify(query))
-      .expect(200);
-    if (query.keyword) {
-      const result = fuse.search(query.keyword);
-      expect(res.body.items).toStrictEqual(result.map(({ item }) => item.depopulate<{ course: string }>().toObject()));
-    }
-
-    query = {
-      keyword: '文學',
-      limit: 100,
-    };
-    res = await request(app)
-      .get('/api/articles/search')
-      .query(qs.stringify(query))
-      .expect(200);
-    if (query.keyword) {
-      const result = fuse.search(query.keyword);
-      expect(res.body.items).toStrictEqual(result.map(({ item }) => item.depopulate<{ course: string }>().toObject()));
+    for (const keyword of ['大學國文', '汪詩珮', '文學']) {
+      const res = await request(app)
+        .get('/api/articles/search')
+        .query(qs.stringify({ keyword, limit: 100 }))
+        .expect(200);
+      const result = fuse.search(keyword).map(({ item }) => item.depopulate<{ course: string }>().toObject());
+      expect(res.body.items).toStrictEqual(result);
     }
   });
 
-  // test the query by course
   it('should support pagination', async () => {
-    let query: ArticleSearchQueryParam | PaginationQueryParam = {
-      tags: ['CHIN'],
-      offset: 0,
-      limit: 100,
-    };
-    let res = await request(app)
-      .get('/api/articles/search')
-      .query(qs.stringify(query))
-      .expect(200);
-    expect(res.body.items).toHaveLength(64);
+    const tags = ['CHIN'];
+    for (const offset of [0, 1, 99, 100, 101]) {
+      for (const limit of [1, 5, 10, 20, 100, 105]) {
+        const res = await request(app)
+          .get('/api/articles/search')
+          .query(qs.stringify({ tags, limit, offset }))
+          .expect(200);
+        expect(res.body.items).toHaveLength(Math.max(0, Math.min(64 - offset, limit)));
+      }
+    }
+  });
 
-    query = {
-      tags: ['CHIN'],
-      limit: 14,
-    };
-    res = await request(app)
-      .get('/api/articles/search')
-      .query(qs.stringify(query))
-      .expect(200);
-    expect(res.body.items).toHaveLength(14);
+  it('should support search with multiple conditions', async () => {
+    const conditions: [string, string[], string[]][] = [
+      ['大學國文', ['CHIN'], []],
+      ['大學國文', ['CHIN', '汪詩珮'], []],
+      ['大學國文', ['CHIN', '文學'], []],
+      ['大學國文', ['CHIN'], ['General']],
+    ];
 
-    query = {
-      tags: ['CHIN'],
-      limit: 15,
-    };
-    res = await request(app)
-      .get('/api/articles/search')
-      .query(qs.stringify(query))
-      .expect(200);
-    expect(res.body.items).toHaveLength(15);
+    for (const [keyword, tags, categories] of conditions) {
+      const fuseOptions = {
+        keys: [
+          'title',
+          'course.names',
+          'course.lecturer',
+        ],
+        threshold: 0.6,
+      };
 
-    query = {
-      tags: ['CHIN'],
-      offset: 10,
-      limit: 3,
-    };
-    res = await request(app)
-      .get('/api/articles/search')
-      .query(qs.stringify(query))
-      .expect(200);
-    expect(res.body.items).toHaveLength(3);
+      const populatedArticles = await models.Article.find().populate<{ course: Course }>('course').exec();
+      const fuse = new Fuse(populatedArticles, fuseOptions);
+      const result = fuse.search(keyword).map(({ item }) => item);
+
+      const filteredResult = result.filter(article =>
+        tags.every(tag => article.tags.includes(tag)),
+      ).filter(article =>
+        categories.every(category => article.course.categories.includes(category)),
+      ).map(article => article.depopulate<{ course: UUID }>().toObject());
+
+      const res = await request(app)
+        .get('/api/articles/search')
+        .query(qs.stringify({ keyword, tags, categories }))
+        .expect(200);
+
+      const articles = ZArticleSchema.array().parse(res.body.items);
+
+      expect(filteredResult).toEqual(expect.arrayContaining(articles));
+    }
   });
 });
