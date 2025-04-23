1733190aeaab8e2dad92d418c594c384ed8a47b4
JerryTsai <b11902040@csie.ntu.edu.tw>
2025-04-23 23:51:42 +0800

    feat(test): add .test.env for for test environment and manage temp directories

--- test/setupFile.ts
+++ test/setupFile.ts
@@ -1,3 +1,5 @@
+import fs from 'fs';
+import path from 'path';
 import { afterAll, beforeAll } from '@jest/globals';
 import dotenv from 'dotenv';
 import { MongoMemoryServer } from 'mongodb-memory-server';
@@ -5,8 +7,19 @@ import mongoose from 'mongoose';
 
 let instance: MongoMemoryServer;
 
+dotenv.config({ path: 'test/.test.env' });
 dotenv.config({ path: '.default.env' });
 dotenv.config({ path: '.env' });
+
+// Add process PID for test temp dirs
+if (process.env.QUIZ_FILE_DIR) {
+  process.env.QUIZ_FILE_DIR = `${process.env.QUIZ_FILE_DIR}_${process.pid.toString()}`;
+}
+if (process.env.ARTICLE_FILE_DIR) {
+  process.env.ARTICLE_FILE_DIR = `${process.env.ARTICLE_FILE_DIR}_${process.pid.toString()}`;
+}
+
+const { env } = await import('@/config.ts');
 
 beforeAll(async () => {
   instance = await MongoMemoryServer.create();
@@ -16,9 +29,19 @@ beforeAll(async () => {
 
   const conn = await mongoose.connect(process.env.MONGO_URI);
   if (conn.connection.db !== undefined) await conn.connection.db.dropDatabase();
+
+  fs.mkdirSync(env.QUIZ_FILE_DIR, { recursive: true });
+  fs.mkdirSync(env.ARTICLE_FILE_DIR, { recursive: true });
+  fs.writeFileSync(
+    path.join(env.ARTICLE_FILE_DIR, '00000002-1131-0000-0000-000000000002.md'),
+    'test',
+    'utf8');
 });
 
 afterAll(async () => {
   await mongoose.disconnect();
+
+  fs.rmSync(env.QUIZ_FILE_DIR, { recursive: true, force: true });
+  fs.rmSync(env.ARTICLE_FILE_DIR, { recursive: true, force: true });
   await instance.stop();
 });
