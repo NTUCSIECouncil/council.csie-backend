7eb4e9f5974c0f4d22f057171f43b3fab447db82
seantsao00 <seantsao00@gmail.com>
2025-05-30 04:32:30 +0800

    feat(types): add optional userId field to Express Request interface

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,9 +1,11 @@
+import { type UUID } from 'crypto';
 import { type UserRecord } from 'firebase-admin/auth';
 
 declare global {
   declare namespace Express {
     export interface Request {
       guser?: UserRecord;
+      userId?: UUID;
       limit?: number;
       offset?: number;
     }
