9b381463357b958f00e17779b6ddf1e2e18f95ce
seantsao00 <seantsao00@gmail.com>
2025-07-20 10:23:40 +0800

    style: fix linting errors

--- src/types/supertest.d.ts
+++ src/types/supertest.d.ts
@@ -2,7 +2,5 @@ import { type Http2ServerRequest, type Http2ServerResponse } from 'http2';
 import { type App as OriginalApp } from 'supertest/types.d.ts';
 
 declare module 'supertest' {
-  export type App =
-    | OriginalApp
-    | ((request: Http2ServerRequest, response: Http2ServerResponse) => Promise<void>);
+  export type App = OriginalApp | ((request: Http2ServerRequest, response: Http2ServerResponse) => Promise<void>);
 }
