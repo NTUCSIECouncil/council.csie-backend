42a92fc50271aef89243982ae7bde904ac238b59
seantsao00 <seantsao00@gmail.com>
2025-08-20 18:33:33 +0800

    chore: remove ESLint stylistic plugins and update to Prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,36 +1,23 @@
 import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
-import importX from 'eslint-plugin-import-x'
-import stylistic from '@stylistic/eslint-plugin'
-import tsParser from '@typescript-eslint/parser'
+import eslintConfigPrettier from "eslint-config-prettier/flat"
 
 
 export default tseslint.config(
   {
     files: ['src/**/*.ts', 'test/**/*.ts', 'scripts/**/*.ts'],
     languageOptions: {
-      // For import-x support
-      parser: tsParser,
-      ecmaVersion: 'latest',
-      sourceType: 'module',
       // For typescript-eslint support
       parserOptions: {
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
       },
     },
-    plugins: {
-      '@stylistic': stylistic,
-    },
     extends: [
       eslint.configs.recommended,
       ...tseslint.configs.strictTypeChecked,
       ...tseslint.configs.stylisticTypeChecked,
-      importX.flatConfigs.typescript,
-      stylistic.configs.customize({
-        semi: true,
-        braceStyle: '1tbs'
-      }),
+      eslintConfigPrettier,
     ],
     rules: {
       'sort-imports': ['warn', { ignoreDeclarationSort: true }],
@@ -39,16 +26,6 @@ export default tseslint.config(
       '@typescript-eslint/no-empty-object-type': ['error', { allowInterfaces: 'with-single-extends' }],
       '@typescript-eslint/use-unknown-in-catch-callback-variable': 'off',
       '@typescript-eslint/consistent-type-imports': ['warn', { fixStyle: 'inline-type-imports' }],
-
-      'import-x/order': ['warn', {
-        groups: ['builtin', 'external', 'internal', 'parent', 'sibling', 'index'],
-        alphabetize: { order: 'asc' },
-      }],
-      'import-x/export': 'error',
-      'import-x/no-duplicates': 'warn',
-      'import-x/consistent-type-specifier-style': ['warn', 'prefer-inline'],
-
-      '@stylistic/max-statements-per-line': 'off',
     },
   },
   {
